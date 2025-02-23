16b1aa793244ae17bc81b5d351b7d0347f2fd413
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-02-23 21:26:16 +0900

    fix: minor config property added

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/p_website_frontend/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, './src')
