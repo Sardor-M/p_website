8bbd2fba24320ded35dcefdfe1dca0411f207e8e
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-02-24 11:05:00 +0900

    fix: backend ip address added

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
 export const API_ENDPOINTS = {
-    BASE_URL: 'http://localhost:3000',
+    BASE_URL: import.meta.env.BACKEND_SERVER_URL,
     BLOG: {
       GET_ALL: '/blog',
       GET_BY_ID: (id: number) => `/blog/${id}`
