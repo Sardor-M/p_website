bf871c6234e00bd9d3985f830dc7774a95defa9d
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-02-27 22:45:01 +0900

    api server url changed

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,8 +1,8 @@
 export const API_ENDPOINTS = {
-    // we set the reverse proxy 
-    BASE_URL: 'https://3.88.85.245',
-    BLOG: {
-      GET_ALL: '/blog',
-      GET_BY_ID: (id: number) => `/blog/${id}`
-    }
-  };
+  // backned url for the api
+  BASE_URL: 'https://api.sardor-m.dev',
+  BLOG: {
+    GET_ALL: '/blog',
+    GET_BY_ID: (id: number) => `/blog/${id}`,
+  },
+};
