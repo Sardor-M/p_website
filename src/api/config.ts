7490e34cbd09b6de895d1adcf60c670fcb709202
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-02-27 18:20:13 +0900

    fix: firebase config changed to handle cors better

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
 export const API_ENDPOINTS = {
     // we set the reverse proxy 
-    BASE_URL: 'https://3.88.85.245.nip.io',
+    BASE_URL: 'https://3.88.85.245',
     BLOG: {
       GET_ALL: '/blog',
       GET_BY_ID: (id: number) => `/blog/${id}`
