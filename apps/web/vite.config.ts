577d6c802f2d1fe7ea5bd55625c758792456f285
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-08-08 18:41:42 +0900

    fix: minor path reading issue in package script fixed

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,52 +3,52 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+    const env = loadEnv(mode, process.cwd(), '');
 
-  return {
-    plugins: [react()],
-    root: './',
-    resolve: {
-      alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
-    },
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      sourcemap: true,
-    },
-    server: {
-      proxy: {
-        '/v1/databases': {
-          target: 'https://api.notion.com',
-          changeOrigin: true,
-          secure: true,
-          headers: {
-            Authorization: `Bearer ${env.VITE_NOTION_API_SECRET}`,
-            'Notion-Version': '2022-06-28',
-            'Content-Type': 'application/json',
-          },
+    return {
+        plugins: [react()],
+        root: './',
+        resolve: {
+            alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
         },
-        '/v1/pages': {
-          target: 'https://api.notion.com',
-          changeOrigin: true,
-          secure: true,
-          headers: {
-            Authorization: `Bearer ${env.VITE_NOTION_API_SECRET}`,
-            'Notion-Version': '2022-06-28',
-            'Content-Type': 'application/json',
-          },
+        build: {
+            outDir: 'dist',
+            assetsDir: 'assets',
+            sourcemap: true,
         },
-        '/v1/blocks': {
-          target: 'https://api.notion.com',
-          changeOrigin: true,
-          secure: true,
-          headers: {
-            Authorization: `Bearer ${env.VITE_NOTION_API_SECRET}`,
-            'Notion-Version': '2022-06-28',
-            'Content-Type': 'application/json',
-          },
+        server: {
+            proxy: {
+                '/v1/databases': {
+                    target: 'https://api.notion.com',
+                    changeOrigin: true,
+                    secure: true,
+                    headers: {
+                        Authorization: `Bearer ${env.VITE_NOTION_API_SECRET}`,
+                        'Notion-Version': '2022-06-28',
+                        'Content-Type': 'application/json',
+                    },
+                },
+                '/v1/pages': {
+                    target: 'https://api.notion.com',
+                    changeOrigin: true,
+                    secure: true,
+                    headers: {
+                        Authorization: `Bearer ${env.VITE_NOTION_API_SECRET}`,
+                        'Notion-Version': '2022-06-28',
+                        'Content-Type': 'application/json',
+                    },
+                },
+                '/v1/blocks': {
+                    target: 'https://api.notion.com',
+                    changeOrigin: true,
+                    secure: true,
+                    headers: {
+                        Authorization: `Bearer ${env.VITE_NOTION_API_SECRET}`,
+                        'Notion-Version': '2022-06-28',
+                        'Content-Type': 'application/json',
+                    },
+                },
+            },
         },
-      },
-    },
-  };
+    };
 });
